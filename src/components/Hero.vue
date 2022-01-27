4248720e5dae47029b00e79a1c4d51b8e177bff8
ManuelAvanzi <manuel.92.avanzi@gmail.com>
2022-01-27 16:42:04 +0100

    Vue Cli

---
+++ src/components/Hero.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="jumbotron">
+        <h2>HERO SECTION</h2>
+    </div>
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+.jumbotron{
+    margin:10px auto;
+    width: 80%;
+    height: 50vh;
+    background-color: rgb(0, 174, 255);
+    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0 0  6px rgba(0, 0, 255, 0.815);
+}
+h2{
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    font-size: 60px;
+}
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="header">
+        <h1>La mia prima app con Vue CLI!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+  .header{
+      
+      margin:0 auto;
+      width: 80%;
+      height: 20vh;
+      background-color: rgba(255, 41, 41, 0.781);
+      box-shadow: 0 0 5px rgba(0, 0, 0, 0.63);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+  }
+  h1{
+      color:white;
+  }
+</style>
